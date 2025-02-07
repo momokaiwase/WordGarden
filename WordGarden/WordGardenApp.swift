ee7d0458a553b742a6eabf31b33f30f1aed653ff
iwasemo <iwasemo@bc.edu>
2025-02-07 15:15:36 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Momoka Iwase on 2025/02/07.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
