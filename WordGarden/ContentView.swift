9e62c17eff085bfb0a66df171e435ac854819625
iwasemo <iwasemo@bc.edu>
2025-02-09 15:10:17 -0500

    Ch. 2.3 Working with TextFields, Keyboards, and FocusState in SwiftUI

--- WordGarden/ContentView.swift
+++ WordGarden/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
     @State private var guessedLetter = ""
     @State private var imageName = "flower8"
     @State private var playAgainHidden = true
+    @FocusState private var textFieldIsFocused: Bool
     var body: some View {
         VStack {
             HStack {
@@ -48,6 +49,18 @@ struct ContentView: View {
                             RoundedRectangle(cornerRadius: 5 )
                                 .stroke(.gray, lineWidth: 2)
                         }
+                        .keyboardType(.asciiCapable)
+                        .submitLabel(.done)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.characters)
+                        .onChange(of: guessedLetter) {
+                            guessedLetter = guessedLetter.trimmingCharacters(in: .letters.inverted)
+                            guard let lastChar = guessedLetter.last else {
+                                    return
+                            }
+                            guessedLetter = String(lastChar).uppercased()
+                        }
+                        .focused($textFieldIsFocused)
                     
                     Button("Guess a Letter:") {
                         //TODO: Guess a letter button action here
@@ -55,6 +68,7 @@ struct ContentView: View {
                     }
                     .buttonStyle(.bordered)
                     .tint(.mint)
+                    .disabled(guessedLetter.isEmpty)
                 }
             } else {
                 Button("Another Word?") {
